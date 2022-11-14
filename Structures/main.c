0bcc4bc0d9735bb67961acb3256e81b8b14398ab
Eli <104906733+Bonnet2B1@users.noreply.github.com>
2022-11-14 17:55:36 +0100

    commit

--- Structures/main.c
+++ Structures/main.c
@@ -1,9 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: edelarbr <edelarbr@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/14 17:54:12 by edelarbr          #+#    #+#             */
+/*   Updated: 2022/11/14 17:54:39 by edelarbr         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "pointhache.h"
 
-int main()
+int	main(void)
 {
-	struct Identite identite;
-	identite.initiale = 'E';
-	identite.age = 21;
-	printf("%c, %d", identite.initiale, identite.age);
+	Identite	utilisateur[2];
+
+	utilisateur[0].name = "Elias";
+	utilisateur[0].age = 21;
+	utilisateur[1].name = "Romain";
+	utilisateur[1].age = 21;
+	printf("%s, %d ans\n", utilisateur[0].name, utilisateur[0].age);
+	printf("%s, %d ans\n", utilisateur[1].name, utilisateur[1].age);
 }
--- Structures/pointhache.h
+++ Structures/pointhache.h
@@ -1,8 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pointhache.h                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: edelarbr <edelarbr@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/14 17:54:18 by edelarbr          #+#    #+#             */
+/*   Updated: 2022/11/14 17:54:19 by edelarbr         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <stdio.h>
 #include <string.h>
 
+typedef struct Identite Identite;
 struct Identite 
 {
-	char	initiale;
+	char	*name;
 	int		age;
 };
