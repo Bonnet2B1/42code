3315e8897c2790a50708cedd59402b2167742845
Eli <104906733+Bonnet2B1@users.noreply.github.com>
2022-11-15 16:12:51 +0100

    commit

--- Structures/main.c
+++ Structures/main.c
@@ -6,32 +6,92 @@
 /*   By: edelarbr <edelarbr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/14 17:54:12 by edelarbr          #+#    #+#             */
-/*   Updated: 2022/11/15 09:16:59 by edelarbr         ###   ########.fr       */
+/*   Updated: 2022/11/15 15:26:31 by edelarbr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pointhache.h"
 
-typedef struct Identite
-{
-	char	*name;
-	int		age;
-} Identite;
+// typedef struct Identite
+// {
+// 	char	*name;
+// 	int		age;
+// } Identite;
 
-void createidentite(Identite *utilisateur)
+// void createidentite(Identite *utilisateur)
+// {
+// 	utilisateur->name = "Romain";
+// 	utilisateur->age = 21;
+// }
+
+// int	main(void)
+// {
+// 	Identite		utilisateur[2];
+
+// 	utilisateur[0].name = "Elias";
+// 	utilisateur[0].age = 21;
+// 	createidentite(&utilisateur[1]);
+
+// 	printf("%s, %d ans\n", utilisateur[0].name, utilisateur[0].age);
+// 	printf("%s, %d ans\n", utilisateur[1].name, utilisateur[1].age);
+// }
+
+typedef struct Element	Element;
+struct					Element
 {
-	utilisateur->name = "Romain";
-	utilisateur->age = 21;
+	int					nombre;
+	Element				*suivant;
+};
+
+typedef struct Liste	Liste;
+struct					Liste
+{
+	Element				*premier;
+};
+
+Liste	*initialisation()
+{
+	Liste *liste = malloc(sizeof(*liste));
+	Element *element = malloc(sizeof(*element));
+
+	if (liste == NULL || element == NULL)
+	{
+		exit(EXIT_FAILURE);
+	}
+
+	element->nombre = 0;
+	element->suivant = NULL;
+	liste->premier = element;
+
+	return (liste);
 }
 
-int	main(void)
+void insertion(Liste *liste, int nvNombre)
 {
-	Identite	 utilisateur[2];
+    /* Création du nouvel élément */
+    Element *nouveau = malloc(sizeof(*nouveau));
+    if (liste == NULL || nouveau == NULL)
+    {
+        exit(EXIT_FAILURE);
+    }
+    nouveau->nombre = nvNombre;
 
-	utilisateur[0].name = "Elias";
-	utilisateur[0].age = 21;
-	createidentite(&utilisateur[1]);
+    /* Insertion de l'élément au début de la liste */
+    nouveau->suivant = liste->premier;
+    liste->premier = nouveau;
+}
 
-	printf("%s, %d ans\n", utilisateur[0].name, utilisateur[0].age);
-	printf("%s, %d ans\n", utilisateur[1].name, utilisateur[1].age);
+void suppression(Liste *liste)
+{
+    if (liste == NULL)
+    {
+        exit(EXIT_FAILURE);
+    }
+
+    if (liste->premier != NULL)
+    {
+        Element *aSupprimer = liste->premier;
+        liste->premier = liste->premier->suivant;
+        free(aSupprimer);
+    }
 }
