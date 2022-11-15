bd185c7fdc88021869b731e0aa7840301afa0082
Eli <104906733+Bonnet2B1@users.noreply.github.com>
2022-11-15 10:14:18 +0100

    structures

--- Structures/main.c
+++ Structures/main.c
@@ -6,20 +6,32 @@
 /*   By: edelarbr <edelarbr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/14 17:54:12 by edelarbr          #+#    #+#             */
-/*   Updated: 2022/11/15 08:28:47 by edelarbr         ###   ########.fr       */
+/*   Updated: 2022/11/15 09:16:59 by edelarbr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pointhache.h"
 
+typedef struct Identite
+{
+	char	*name;
+	int		age;
+} Identite;
+
+void createidentite(Identite *utilisateur)
+{
+	utilisateur->name = "Romain";
+	utilisateur->age = 21;
+}
+
 int	main(void)
 {
-	struct Identite	utilisateur[2];
+	Identite	 utilisateur[2];
 
 	utilisateur[0].name = "Elias";
 	utilisateur[0].age = 21;
-	utilisateur[1].name = "Romain";
-	utilisateur[1].age = 21;
+	createidentite(&utilisateur[1]);
+
 	printf("%s, %d ans\n", utilisateur[0].name, utilisateur[0].age);
 	printf("%s, %d ans\n", utilisateur[1].name, utilisateur[1].age);
 }
--- Structures/pointhache.h
+++ Structures/pointhache.h
@@ -6,15 +6,10 @@
 /*   By: edelarbr <edelarbr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/14 17:54:18 by edelarbr          #+#    #+#             */
-/*   Updated: 2022/11/15 08:28:47 by edelarbr         ###   ########.fr       */
+/*   Updated: 2022/11/15 08:31:36 by edelarbr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include <string.h>
 
-typedef struct Identite
-{
-	char	*name;
-	int		age;
-} Identite;
