a4862736d630f79eeef7fb7cf3166a9cbb926e8e
Eli <104906733+Bonnet2B1@users.noreply.github.com>
2022-11-29 15:31:11 +0100

    printf finito

--- Cursus/printf/ft_printf.c
+++ Cursus/printf/ft_printf.c
@@ -6,49 +6,51 @@
 /*   By: edelarbr <edelarbr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/22 15:53:09 by edelarbr          #+#    #+#             */
-/*   Updated: 2022/11/29 14:57:25 by edelarbr         ###   ########.fr       */
+/*   Updated: 2022/11/29 15:29:56 by edelarbr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-static int	selectfunction(char **topf, int ret, va_list ap)
+static void	selectfunction(char topf, int *ret, va_list ap)
 {
-	if (**topf == '%' && ret >= 0)
-	{
-		*topf++;
-		if (**topf == '%')
-			ret += ft_putchar_pf('%');
-		else if (**topf == 'c')
-			ret += ft_putchar_pf(va_arg(ap, int));
-		else if (**topf == 's')
-			ret += ft_putstr_pf(va_arg(ap, char *));
-		else if (**topf == 'p')
-			ret += ft_putadress_pf(va_arg(ap, unsigned long long));
-		else if (**topf == 'd' || **topf == 'i')
-			ret += ft_putnbr_pf(((long long)va_arg(ap, int)));
-		else if (**topf == 'u')
-			ret += ft_putnbr_pf(((long long)va_arg(ap, unsigned int)));
-		else if (**topf == 'x')
-			ret += ft_putnbr_hex_min_pf(va_arg(ap, unsigned int));
-		else if (**topf == 'X')
-			ret += ft_putnbr_hex_maj_pf(va_arg(ap, unsigned int));
-	}
-	else if (ret >= 0)
-		ret += ft_putchar_pf(**topf);
-	*topf++;
-	return (ret);
+	if (topf == '%')
+		*ret += ft_putchar_pf('%');
+	else if (topf == 'c')
+		*ret += ft_putchar_pf(va_arg(ap, int));
+	else if (topf == 's')
+		*ret += ft_putstr_pf(va_arg(ap, char *));
+	else if (topf == 'p')
+		*ret += ft_putadress_pf(va_arg(ap, unsigned long long));
+	else if (topf == 'd' || topf == 'i')
+		*ret += ft_putnbr_pf(((long long)va_arg(ap, int)));
+	else if (topf == 'u')
+		*ret += ft_putnbr_pf(((long long)va_arg(ap, unsigned int)));
+	else if (topf == 'x')
+		*ret += ft_putnbr_hex_min_pf(va_arg(ap, unsigned int));
+	else if (topf == 'X')
+		*ret += ft_putnbr_hex_maj_pf(va_arg(ap, unsigned int));
 }
 
-int	ft_printf(const char **topf, ...)
+int	ft_printf(const char *topf, ...)
 {
 	va_list	ap;
 	int		ret;
 
 	ret = 0;
-	va_start(ap, *topf);
+	va_start(ap, topf);
 	while (*topf && ret >= 0)
-		ret += selectfunction((char *) &topf, ret, ap);
+	{
+		if (*topf == '%' && ret >= 0)
+		{
+			topf++;
+			selectfunction(*topf, &ret, ap);
+		}
+		else if (ret >= 0)
+			ret += ft_putchar_pf(*topf);
+		topf++;
+	}
+	va_end(ap);
 	if (ret < 0)
 		return (-1);
 	return (ret);
