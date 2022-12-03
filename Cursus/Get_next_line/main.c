0def634218c0f116f49e7195f2ff92e00242adf7
Eli <104906733+Bonnet2B1@users.noreply.github.com>
2022-12-03 15:44:31 +0100

    save

--- Cursus/Get_next_line/main.c
+++ Cursus/Get_next_line/main.c
@@ -1,31 +1,32 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: edelarbr <edelarbr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/11/30 14:59:39 by edelarbr          #+#    #+#             */
-/*   Updated: 2022/12/03 15:44:25 by edelarbr         ###   ########.fr       */
+/*   Created: 2022/12/03 15:19:11 by edelarbr          #+#    #+#             */
+/*   Updated: 2022/12/03 15:31:01 by edelarbr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+#include <fcntl.h>
+#include <stdio.h>
 
-char	*get_next_line(int fd)
+int main()
 {
-	char *storage;
-	char *buf;
-	static char **tab;
-	buf = malloc(sizeof(char) * (BUFFER_SIZE + 1));
-	storage = NULL;
-	while(read(fd, buf, BUFFER_SIZE) > 0)
+	int fd;
+	char *line;
+
+	fd = open("text.txt", O_RDONLY);
+
+	while (1)
 	{
-		buf[BUFFER_SIZE] = '\0';
-		storage = ft_strjoin(storage, buf);
+		line = get_next_line(fd);
+		if (line == NULL)
+			break;
+		printf("%s", line);
 	}
-	tab = ft_split(storage, '\n');
-	while(*tab)
-		return (*tab++);
-	return (NULL);
+	return 0;
 }
