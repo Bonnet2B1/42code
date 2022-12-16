b951f65a5ba5a2439c73de9e34df6140e3ee4619
Eli <104906733+Bonnet2B1@users.noreply.github.com>
2022-12-16 20:38:20 +0100

    save

---
+++ Cursus/Get_next_line_w_gpt/get_next_line.h
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: edelarbr <edelarbr@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/30 15:00:03 by edelarbr          #+#    #+#             */
+/*   Updated: 2022/12/05 17:24:23 by edelarbr         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 5
+# endif
+
+char	*get_next_line(int fd);
+char	*ft_strjoin(char const *s1, char const *s2);
+size_t	ft_strlen(const char *s);
+char	**ft_split(const char *s, char c);
+char	**freeall(char **tab, size_t indice);
+size_t	wordcount(const char *s, char c);
+
+
+#endif
